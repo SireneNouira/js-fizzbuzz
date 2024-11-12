b517b57eaf69eed3530a3f7eb6f7bc0e987ba8e3
SireneNouira <sirene.nouira16@gmail.com>
2024-11-12 14:07:58 +0100

    fizz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,26 @@
-let nombre = 30;
+//let nombre = 5;
 
-if (nombre % 3 === 0 && nombre % 5 != 0) {
-  console.log("fizz");
-} else if (nombre % 5 === 0 && nombre % 3 != 0) {
-  console.log("buzz");
-}
+// if (nombre % 3 === 0 && nombre % 5 != 0) {
+//   console.log("fizz");
+// } else if (nombre % 5 === 0 && nombre % 3 != 0) {
+//   console.log("buzz");
+// }
 
-if (nombre % 3 === 0 && nombre % 5 === 0) {
-  console.log("fizzbuzz");
-} else if (nombre % 5 != 0 && nombre % 3 != 0) {
-  console.log(nombre);
-}
+// if (nombre % 3 === 0 && nombre % 5 === 0) {
+//   console.log("fizzbuzz");
+// } else if (nombre % 5 != 0 && nombre % 3 != 0) {
+//   console.log(nombre);
+// }
+
+// let printable = "";
+
+// if (nombre % 3 === 0) {
+//   printable += "fizz";
+// }
+
+// if (nombre % 5 === 0) {
+//   printable += "buzz";
+// }
+
+// console.log(printable || nombre);
+
